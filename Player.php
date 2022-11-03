b131f73c924924ff9cdaaecf083c506bd537c771
Henry <henryppercy@gmail.com>
2022-11-03 15:26:10 +0000

    Created .gitignore

--- Player.php
+++ Player.php
@@ -4,5 +4,19 @@ class Player
 {
     private array $hand = [];
 
+    /**
+     * @return array
+     */
+    public function getHand(): array
+    {
+        return $this->hand;
+    }
 
+    /**
+     * @param array $hand
+     */
+    public function setHand(array $hand): void
+    {
+        $this->hand = $hand;
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 require_once 'Deck.php';
 
 $testDeck = new Deck();
-
 $deck = $testDeck->getDeck();
 
 echo '<pre>';
 print_r($deck);
 echo '</pre>';
+
+
+function shuffle($deck)
+{
+    return shuffle($deck);
+}
+
+function dealCards()
+{
+    
+}
+
+function playGame()
+{
+
+}
