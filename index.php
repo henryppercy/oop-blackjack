0571d6208a0dd8e1aefe502b63f5161098bd41e2
Henry <henryppercy@gmail.com>
2022-11-03 16:53:51 +0000

    Added favicon

--- index.php
+++ index.php
@@ -11,9 +11,6 @@ require_once 'classes/Game.php';
     <link rel="stylesheet" href="styles/normalize.css">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="shortcut icon" type="image/png" href="images/favicon.png">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Vidaloka&display=swap" rel="stylesheet">
 </head>
 
 <?php
