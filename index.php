c7f90109dcdfff07f4b965e1e21b330ea1747008
Henry <henryppercy@gmail.com>
2022-11-04 08:55:16 +0000

    Added two player view

--- index.php
+++ index.php
@@ -12,8 +12,34 @@ require_once 'classes/Game.php';
     <link rel="stylesheet" href="styles/style.css">
     <link rel="shortcut icon" type="image/png" href="images/favicon.png">
 </head>
+<body>
+    <header>
+        <h1>♠♥ Welcome to Blackjack ♦♣</h1>
+    </header>
+    <main>
+        <section class="players">
+            <div class="card player-one">
+                <h2>Player One</h2>
+                <p>Ten of Clubs</p>
+                <p>Jack of Diamonds</p>
+                <p>Score: 21</p>
+            </div>
+            <div class="card player-two">
+                <h2>Player Two</h2>
+                <p>Ace of Spades</p>
+                <p>Three of Hearts</p>
+                <p>Score: 14</p>
+            </div>
+        </section>
+        <section class="total">
+            <div class="card">
+                <h3>Player One Wins!</h3>
+            </div>
+        </section>
+    </main>
+</body>
 
 <?php
-$game = new Game;
-echo $game->playGame();
-?>
+//$game = new Game;
+//echo $game->playGame();
+//?>
